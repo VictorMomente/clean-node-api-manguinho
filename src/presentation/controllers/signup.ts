e8f10b38a2b7e3a8e176a495e00c629ff1f349de
VictorMomente <victor.h.momente@gmail.com>
2021-05-27 21:53:10 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,17 @@
 // claase SignUp Controller
+// Dúvidas sobre o badRequest e MissingParamError? olhar aula 7 seção 2
+
 import { httpResponse, httpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }

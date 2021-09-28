f169455e5a43cc0195ffdb1bf98389d864eec0a3
VictorMomente <victor.h.momente@gmail.com>
2021-09-27 22:03:22 -0300

    refactor: connect to mongodb before integration tests

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app'
 
-app.listen('5050', () => console.log('Server is running at https://localhost:5050'))
+app.listen('5050', () => console.log('Server is running at http://localhost:5050'))
